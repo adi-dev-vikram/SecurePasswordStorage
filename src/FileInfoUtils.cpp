e74f0d35b579704bdbfb1b189e5a57c05f3b78fa
Aditya Vikram <65796572+adi-dev-vikram@users.noreply.github.com>
2022-09-27 19:53:16 +0530

    First commit

--- src/FileInfoUtils.cpp
+++ src/FileInfoUtils.cpp
@@ -6,88 +6,17 @@ using namespace std;
 #include <sstream>
 #include <unistd.h>
 #include <sys/types.h>
-uid_t getuid(void);
-uid_t geteuid(void);
 
-int num;
-// global value = 0
-int main()
+std::string GetFilePath()
 {
     
-    int val;
-    
-    printf("This process is %d\n", getpid());
-    printf("The real user ID is %d\n", getuid());
-    printf("The effective user ID is %d\n", geteuid());
-    
-    
-    if((val=fork())<0){
-        printf("Problem creating new process\n");
-        return 1;
-    }
-    //fork returns 0 to newly created process
-    if(val==0){
-        printf("This process is %d\n", getpid());
-        printf("The real user ID is %d\n", getuid());
-        printf("The effective user ID is %d\n", geteuid());
-    }
-    
-    // rearrange elements of given array in high and low manner
-    // step 1: sort them in ascending order
-    // step 2: intialize i = 0 and j=n-1
-    // create a new aaray with k=0
-    // a[k++] = b[i++]
-    // a[k++] = b[j--]
-    
-    int arrNum[] = { 1,5,3,2,9,7};
-    int n = sizeof(arrNum)/sizeof(int);
-    sort(arrNum, arrNum+n);
-    int k = 0;
-    int* auxNewArray = new int[10];
-    
-    if (NULL==auxNewArray) {
-        cout<< "Intialize failed"<<endl;
-        return -1;
-    }
-    
-    int i=0, j=n-1;
-    while (i<j) {
-        auxNewArray[k++]=arrNum[i++];
-        auxNewArray[k++]=arrNum[j--];
-    }
-    for (int v=0; v<k; v++) {
-        cout << *(auxNewArray+v)<<endl;
-    }
-    
-    
-    const char *pText = "FirstString" "SecondString";
-    cout << pText <<endl;// same as std::string
-    
-    
-    std::string newStr = "StringNumbaer";
-    std::string concat = newStr + newStr;
-    cout << concat << endl;
-    // a byte is 8 bits
-    constexpr size_t byte = 8;
-    
-    cout << num <<endl;
-    
-    printf("size of char is %ld bits\n And %ld for int", sizeof(char) * 
-byte, sizeof(int)*byte);
-    // char is 1 byte and int is 4 byte on macos ( might be diff)
-    // for #include<cstdint> we get int8_t and others which are std size 
-for all
-    
-    
-    // use auto to deduce variables
-    
-    // auto ite = numbers.begin()
-    // useful for complex objects
-    
-    
+}
+
+bool FetchKeyVal()
+{
 // Read Key-value pairs from config files
 // For example: AuthServers=<IP>
-    
+    std::map<std::string, std::string> ConfigInfo;
     std::ifstream fs;
     fs.open("pwd.txt");
     if(fs.good())
@@ -97,8 +26,7 @@ for all
     else{
         printf("aditya");
     }
-    std::map<std::string, std::string> ConfigInfo;
-
+    
     std::string line;
     while (std::getline(fs, line))
     {
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,46 @@
-//here
+#include<iostream>>
+
+#include "FileInfoUtils.h"
+#include "Encrypter.h"
+#include<iostream>
+using namespace std;
+#include<cstring>
+#include<map>
+#include<fstream>
+#include <sstream>
+#include <unistd.h>
+#include <sys/types.h>
+uid_t getuid(void);
+uid_t geteuid(void);
+
+
+int main()
+{
+    printf("This process is %d\n", getpid());
+    printf("The real user ID is %d\n", getuid());
+    printf("The effective user ID is %d\n", geteuid());
+    
+    
+    auto me = getuid();
+    auto myprivs = geteuid();
+
+
+    if (me == myprivs)
+    {
+        std::cout << "Running as self\n";
+    } 
+    else
+    {
+        std::cout << "Running as somebody else\n";
+        if (getuid()) 
+        {
+            printf("%s", "You are not root!\n");
+        }
+        else
+        {
+            printf("%s", "OK, you are root.\n");
+        } 
+        
+    }
+    
+
